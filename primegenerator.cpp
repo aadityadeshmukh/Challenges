dc04791564554ed3f6a44e59a78549343272790d
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2015-08-29 11:31:47 +0530

    Fixes #1 Program complete
    Wrote the complete program for the prime generator within bounds. Need
    to debug and fix some issues

--- primegenerator.cpp
+++ primegenerator.cpp
@@ -6,8 +6,56 @@ using namespace std;
 int Optimus(int BC1, int BC2)
 {
 	cout<<"\n\n*******";
-	vector<int>
+	vector<int> pp, sp ,testprimes;
+	//Get a list of potential pries by running a even number filter
+	for(int i=BC1;i<=BC2;i++)
+		if(i%2 != 0)
+			pp.push_back(i);
 
+	//Now find all the test primes before the 1st potential primes
+	testprimes.push_back(2);
+	for(int i=3;i<=pp[0];i++)
+	{
+		if(i%2 != 0) //only check odd values
+			{
+				int testsize = testprimes.size();
+				bool isPrime = true;
+				for(int j=0;j<testsize;j++)
+				{
+					if(i % testprimes[j]==0)
+					{
+						isPrime = false;
+						break;
+					}
+				}
+
+				if(isPrime)
+					testprimes.push_back(i);
+			}
+	}
+
+	//Now test all potential primes with test primes
+	for(int i=0;i<pp.size();i++)
+	{
+		bool isPrime = true;
+        for(int j=0;j<testprimes.size();j++)
+        {
+        	if(pp[i] % testprimes[j] == 0)
+        	{
+        		isPrime = false;
+        		break;
+        	}	
+        }
+        if(isPrime)
+        {
+        	sp.push_back(pp[i]);
+        	testprimes.push_back(pp[i]);
+        }
+	}
+
+	//Print the numbers
+	for(int i=0;i<sp.size();i++)
+		cout<<"\t"<<sp[i];
 }
 
 int main()
@@ -30,7 +78,7 @@ int main()
          int bound1(0),bound2(0);
          cin>>bound1;
          cin>>bound2;
-         if(bound1 < 1 || bound2 < 1 || bound1 > 1000000000 || bound2 > 1000000000 || (bound2 - bound1) <= 100000 )
+         if(bound1 < 1 || bound2 < 1 || bound1 > 1000000000 || bound2 > 1000000000 || (bound2 - bound1) >= 100000 )
          {
          	cout<<"Invalid input. Exiting"<<endl;
          	return 0;
