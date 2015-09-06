48aa1492ef9ee29f5bb2ccb11316801556e8a9a5
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2015-09-06 12:43:22 +0530

    Tried to code but need more info
    Tried to code but need more info on RPN. The concept is not that easy

--- RPN/rpn.cpp
+++ RPN/rpn.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <conio.h>
 #include <vector>
+#include <ctype.h>
 using namespace std;
 int main()
 {
@@ -12,11 +13,29 @@ int main()
 	cin>>tc;
 
 	//Taking expressions as inputs
-	vector < std::string > iVecExpressions;  
+	vector < std::string > iVecExpressions, oVecVars , oVecOperators;  
 	for(int i = 0; i<tc ; i++)
 	{
-		std::string inpString;
+		std::string inpString, outAlphaString , outOperatorString;
 		cout << "\n Enter the " << i+1 << "th expression:";
         cin >> inpString;
+        for(int j=0; j<inpString.length(); j++)
+        {
+        	if(int iA = isAlpha(inpString[j]))
+        	{
+        		outAlphaString += inpString[j];
+        	}
+        	else
+        	{
+        		
+        	}
+
+        }
+	}
+
+	//Phase 2 seperating the variables
+	for(int i=0; i<tc; i++)
+	{
+		std::string inpString = iVec
 	}
 }
